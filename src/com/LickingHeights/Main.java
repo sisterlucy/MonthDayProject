b49a9d6a42375356d77b5ed85ec0a8d003b4f22b
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-04-25 13:39:30 -0400

    Before i asked for help

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,28 +1,41 @@
 package com.LickingHeights;
 
+import java.time.DayOfWeek;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    //create a method for determining  month day they were born
-    //create a method for determining the day of the week they wee born
-    //create a method for determining the line of the poem
-    //wrap the code in a loop
+        //create a method for determining  month day they were born
+        //create a method for determining the day of the week they wee born
+        //create a method for determining the line of the poem
+        //wrap the code in a loop
         Scanner keyboard;
-        String month;
-        String day;
+
         System.out.println("There is an old nursery rhyme that that goes like this...");
         System.out.println("Monday's child is fair of face,\n" +
-            "Tuesday's child is full of grace,\n" +
-            "Wednesday's child is full of woe,\n" +
-            "Thursday's child has far to go.\n" +
-            "Friday's child is loving and giving,\n" +
-            "Saturday's child works hard for a living,\n" +
-            "But the child born on the Sabbath Day,\n" +
-            "Is fair and wise and good in every way.\n");
-    System.out.println("Let's see what day of the of the week you were born on");
-    System.out.println("What month were you born in? (enter as number)");
-
+                "Tuesday's child is full of grace,\n" +
+                "Wednesday's child is full of woe,\n" +
+                "Thursday's child has far to go.\n" +
+                "Friday's child is loving and giving,\n" +
+                "Saturday's child works hard for a living,\n" +
+                "But the child born on the Sabbath Day,\n" +
+                "Is fair and wise and good in every way.\n");
+        System.out.println("Let's see what day of the of the week you were born on");
+        monthAndDay();
     }
+    static void monthAndDay(){
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        System.out.println("What month were you born in? (enter as number)");
+        String month= keyboard.nextLine();
+        System.out.println("What was the date you were born on?");
+        String date= keyboard.nextLine();
+        System.out.println("What year were you born in?");
+        String year= keyboard.nextLine();
+        System.out.println(month);
+        System.out.println(date);
+        System.out.println(year);
+    }
+
 }
