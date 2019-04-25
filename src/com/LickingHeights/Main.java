6982456746f8f60f077d56329c14d40eb519fd55
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-04-25 13:14:51 -0400

    added the poem

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,28 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //create a method for determining  month day they were born
+    //create a method for determining the day of the week they wee born
+    //create a method for determining the line of the poem
+    //wrap the code in a loop
+        Scanner keyboard;
+        String month;
+        String day;
+        System.out.println("There is an old nursery rhyme that that goes like this...");
+        System.out.println("Monday's child is fair of face,\n" +
+            "Tuesday's child is full of grace,\n" +
+            "Wednesday's child is full of woe,\n" +
+            "Thursday's child has far to go.\n" +
+            "Friday's child is loving and giving,\n" +
+            "Saturday's child works hard for a living,\n" +
+            "But the child born on the Sabbath Day,\n" +
+            "Is fair and wise and good in every way.\n");
+    System.out.println("Let's see what day of the of the week you were born on");
+    System.out.println("What month were you born in? (enter as number)");
+
     }
 }
